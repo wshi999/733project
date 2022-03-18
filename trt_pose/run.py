a768ca68a412879ad7ddecc764ac60d735ecfd64
Wenhao Shi <wenhao_shi@sfu.ca>
2022-03-18 16:20:01 -0700

    demo

--- trt_pose/run.py
+++ trt_pose/run.py
@@ -3,15 +3,17 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from util import coco_category_to_topology
-from numpy.lib.stride_tricks import as_strided
 from model import resnet18_baseline_att
 import numpy as np
 import torch
 import PIL
 import torchvision
+from scipy.ndimage.filters import gaussian_filter
+import math
 
 MODELPATH = "model/hand_pose_resnet18_att_244_244.pth"
-IMG = "images/hand.jpg"
+IMG = "images/demo.jpg"
+threshold = 0.03  # 0.1, 0.3
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -35,48 +37,15 @@ def preprocess(image):
     return image[None, ...]
 
 
-def pool2d(A, kernel_size, stride, padding, pool_mode="max"):
-    """
-    2D Pooling
-    Parameters:
-        A: input 2D array
-        kernel_size: int, the size of the window
-        stride: int, the stride of the window
-        padding: int, implicit zero paddings on both sides of the input
-        pool_mode: string, 'max' or 'avg'
-    """
-    # Padding
-    A = np.pad(A, padding, mode="constant")
-
-    # Window view of A
-    output_shape = (
-        (A.shape[0] - kernel_size) // stride + 1,
-        (A.shape[1] - kernel_size) // stride + 1,
-    )
-    kernel_size = (kernel_size, kernel_size)
-    A_w = as_strided(
-        A,
-        shape=output_shape + kernel_size,
-        strides=(stride * A.strides[0], stride * A.strides[1]) + A.strides,
-    )
-    A_w = A_w.reshape(-1, *kernel_size)
-
-    # Return the result of pooling
-    if pool_mode == "max":
-        return A_w.max(axis=(1, 2)).reshape(output_shape)
-    elif pool_mode == "avg":
-        return A_w.mean(axis=(1, 2)).reshape(output_shape)
-
-
-def nms(heatmaps):
-    results = np.empty_like(heatmaps)
-    for i in range(heatmaps.shape[0]):
-        heat = heatmaps[i, :, :]
-        hmax = pool2d(heat, 3, 1, 1)
-        keep = (hmax == heat).astype(float)
-
-        results[i, :, :] = heat * keep
-    return results
+def postprocess(heatmap, orig_shape):
+    peaks = np.zeros((21, 2))
+    for i in range(21):
+        heat = heatmap[i, :, :]
+        heat = gaussian_filter(heat, sigma=3)
+        peak = np.unravel_index(heat.argmax(), heat.shape)
+        if heat[peak] > threshold:
+            peaks[i] = peak
+    return peaks * (orig_shape[0] / heatmap.shape[1], orig_shape[1] / heatmap.shape[2])
 
 
 topology = coco_category_to_topology(hand_pose)
@@ -84,8 +53,9 @@ num_parts = len(hand_pose["keypoints"])
 num_links = len(hand_pose["skeleton"])
 model = resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
 model.load_state_dict(torch.load(MODELPATH))
-img = cv2.imread(IMG)
-img = preprocess(img)
+ori_img = cv2.imread(IMG)
+ori_shape = ori_img.shape[0:2]
+img = preprocess(ori_img)
 cmap, paf = model(img)
 
 print(cmap.shape, paf.shape)
@@ -95,5 +65,15 @@ for i in range(heatmap.shape[0]):
     plt.imshow(heatmap[i, :, :])
     plt.savefig(f"outputs/heatMat{i}.png")
 
-peaks = nms(heatmap)
-print(peaks.shape)
+peaks = postprocess(heatmap, ori_shape)
+print(peaks)
+out = ori_img.copy()
+for peak in peaks:
+    out = cv2.circle(
+        out,
+        (round(peak[1]), round(peak[0])),
+        radius=4,
+        color=(0, 0, 255),
+        thickness=-1,
+    )
+cv2.imwrite("outputs/out.jpg", out)
--- trt_pose/util.py
+++ trt_pose/util.py
@@ -1,7 +1,51 @@
 import torch
 import cv2
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
-import tf_pose.pafprocess.pafprocess as pafprocess
+
+def pool2d(A, kernel_size, stride, padding, pool_mode="max"):
+    """
+    2D Pooling
+    Parameters:
+        A: input 2D array
+        kernel_size: int, the size of the window
+        stride: int, the stride of the window
+        padding: int, implicit zero paddings on both sides of the input
+        pool_mode: string, 'max' or 'avg'
+    """
+    # Padding
+    A = np.pad(A, padding, mode="constant")
+
+    # Window view of A
+    output_shape = (
+        (A.shape[0] - kernel_size) // stride + 1,
+        (A.shape[1] - kernel_size) // stride + 1,
+    )
+    kernel_size = (kernel_size, kernel_size)
+    A_w = as_strided(
+        A,
+        shape=output_shape + kernel_size,
+        strides=(stride * A.strides[0], stride * A.strides[1]) + A.strides,
+    )
+    A_w = A_w.reshape(-1, *kernel_size)
+
+    # Return the result of pooling
+    if pool_mode == "max":
+        return A_w.max(axis=(1, 2)).reshape(output_shape)
+    elif pool_mode == "avg":
+        return A_w.mean(axis=(1, 2)).reshape(output_shape)
+
+
+def nms(heatmaps):
+    results = np.empty_like(heatmaps)
+    for i in range(heatmaps.shape[0]):
+        heat = heatmaps[i, :, :]
+        hmax = pool2d(heat, 3, 1, 1)
+        keep = (hmax == heat).astype(float)
+
+        results[i, :, :] = heat * keep
+    return results
 
 
 def coco_category_to_topology(coco_category):
